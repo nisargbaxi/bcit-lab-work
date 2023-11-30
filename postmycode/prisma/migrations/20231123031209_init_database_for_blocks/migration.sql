e09dcd6dd5ca4caadd1a99d423b792fb7a4dc6f7
Nisarg Baxi <nisargbaxi@gmail.com>
2023-11-29 19:16:41 -0800

    Delete postmycode/prisma/migrations directory

--- postmycode/prisma/migrations/20231123031209_init_database_for_blocks/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateTable
-CREATE TABLE "codeblock" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "title" TEXT NOT NULL,
-    "code" TEXT NOT NULL,
-    "rating" INTEGER NOT NULL
-);
